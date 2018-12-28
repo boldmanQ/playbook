ee2500048dda725bd40fe09448f2ce98c357795f
zhangqian <zhangqian@inad.com>
2018-12-28 17:23:54 +0800

    create repo, add all file

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#ansible-playbook -i hosts --ask-vault-pass -u root --private-key ~/.ssh/id_rsa --vault-password-file=~/ansible-vault-pass $*
+ansible-playbook -i hosts -u root --private-key ~/.ssh/id_rsa --vault-password-file=~/ansible-vault-pass $*
